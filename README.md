16f720718ea07422d5266a1a164853fafa62e37b
Jaesang Oh <62362542+jaeoh91@users.noreply.github.com>
2026-09-13 17:46:38 -0400

    feat: migrate to mysql

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Project 1 for CS2340
 
 Reference: https://galileo-gatech.primo.exlibrisgroup.com/discovery/fulldisplay?context=L&vid=01GALI_GIT:GT&docid=alma9915270455502947
+
+# Run
+- XAAMP Dashboard : http://localhost/dashboard/
+- App: 
+- Run XAAMP
--- moviesstore/moviesstore/settings.py
+++ moviesstore/moviesstore/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'moviesstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'moviesstore',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -127,3 +131,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / 'moviesstore/static/',
 ]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
